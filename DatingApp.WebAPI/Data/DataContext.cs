0bcf14c07dd729ae2f00e034148f05f44cc4093a
Balaji <lingabalaji17@gmail.com>
2019-12-14 15:13:22 +0530

    Initial Commit of Dating App

---
+++ DatingApp.WebAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using DatingApp.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values{ get; set; }
+    }
+}
